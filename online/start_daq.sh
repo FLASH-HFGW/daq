5ccffac5c251cfc02e8afc2103a08dd4cad11294
GioDho <giorgio.dho@gssi.it>
2025-10-23 12:29:39 +0200

    Migration to new folder and path adjustment

---
+++ online/start_daq.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+./kill_daq.sh
+
+odbedit -c clean
+
+mhttpd -D
+sleep 2
+mlogger -D
+
+echo Please point your web browser to http://localhost:8081
+echo Or run: mozilla http://localhost:8081 &
+echo To look at live histograms, run: roody -Hlocalhost
+
+#end file

ce19004febfa921fe47be9454a33bc58b4757dda
GioDho <giorgio.dho@gssi.it>
2026-01-30 16:10:29 +0100

    Addition of compilation files for Spectrum lib

--- online/bin/compile_fe.sh
+++ online/bin/compile_fe.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo $1
+if [ "$1" = "clean" ]; then
+    rm -r /home/cold/daq/build-fe/*
+fi;
 cmake -B /home/cold/daq/build-fe/ -S /home/cold/daq/online/source/fe/
 cmake --build /home/cold/daq/build-fe/
 cmake --install /home/cold/daq/build-fe/ --prefix /home/cold/daq/online/bin/ 

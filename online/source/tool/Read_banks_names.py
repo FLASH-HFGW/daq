2ec953a8a9256f86bcd0dd63e3969c7360d04d39
GioDho <giorgio.dho@gssi.it>
2026-06-24 15:30:16 +0200

    Addition of more details in bank fast reader

--- online/source/tool/Read_banks_names.py
+++ online/source/tool/Read_banks_names.py
@@ -7,11 +7,16 @@ import midas
 import midas.file_reader
 
 
-mf = midas.file_reader.MidasFile("../../data/run00247.mid.gz")
+mf = midas.file_reader.MidasFile("../../../../data/run00373.mid.gz")
 for mevent in mf:
     if mevent.header.is_midas_internal_event():
             continue
     else:
             keys = mevent.banks.keys()
+            print(keys, len(keys), mevent.header.serial_number)
     for iobj,key in enumerate(keys):
             print(key, mevent.header.event_id)
+            if key=='TGPS':
+                arr =mevent.banks[key].data
+                time = ''.join(chr(x) for x in arr if x !=0)
+                print('scrivo ',time)
